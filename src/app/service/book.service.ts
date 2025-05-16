fa1d895cbf47e32c16f436d496b318a3682268bc
DumithLusena <dumithlusena123@gmail.com>
2025-05-16 13:42:36 +0530

    create at services

--- src/app/service/book.service.ts
+++ src/app/service/book.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/book';
+
+  constructor(private http: HttpClient) { }
+
+  getBooks(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+
+  getBook(isbn: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${isbn}`);
+  }
+
+  isAvailable(isbn: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${isbn}/available`);
+  }
 }
--- src/app/service/borrow.service.ts
+++ src/app/service/borrow.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BorrowService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/borrow';
+
+  constructor(private http: HttpClient) { }
+
+   getAll(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+
+  getActiveByUser(userId: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/user/${userId}/active`);
+  }
+
+  issueBook(data: { userId: number, isbn: string }): Observable<any> {
+    return this.http.post(`${this.baseUrl}/issue`, data);
+  }
+
+  returnBook(data: { userId: number, isbn: string }): Observable<any> {
+    return this.http.post(`${this.baseUrl}/return`, data);
+  }
+
 }

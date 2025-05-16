c6bec1c2571071bc829d56c8435b54eeaed88c41
DumithLusena <dumithlusena123@gmail.com>
2025-05-16 14:06:27 +0530

    created at component and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UserComponent } from './pages/user/user.component';
+import { BookComponent } from './pages/book/book.component';
+import { BorrowDetailsComponent } from './pages/borrow-details/borrow-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component:DashboardComponent
+  },
+  {
+        path:"dashboard",
+        component:DashboardComponent,
+        children:[
+            {
+                path:"user",
+                component:UserComponent
+            },
+            {
+                path:"view-books",
+                component:BookComponent
+            },
+            {
+              path:"borrow-details",
+              component:BorrowDetailsComponent
+            }
+        ]
+  }
+];
---
+++ src/app/pages/borrow-details/borrow-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-borrow-details',
+  imports: [],
+  templateUrl: './borrow-details.component.html',
+  styleUrl: './borrow-details.component.css'
+})
+export class BorrowDetailsComponent {
+
+}
